b5fb5aabcd82aa0f57f7bcda644ad04a28435bef
Shirak22 <116073530+Shirak22@users.noreply.github.com>
2022-12-09 14:09:16 +0100

    Javascript sticky navBar on resize and scrol

--- js/script.js
+++ js/script.js
@@ -1,13 +1,55 @@
 
-let navBar = document.querySelector('.navbar__wrapper'); 
+let navBar = document.querySelector('.navbar__wrapper');
+let navBarLogo = document.querySelector('.navbar__logo');
+let windowWidth = window.innerWidth; 
 
-
-window.onscroll = ()=> {
-    
-    if(document.documentElement.scrollTop > 100 || document.body.scrollTop > 100 ){
-        navBar.classList.add('navbar--onscroll');
+//NavBar action on scroll 
+window.addEventListener('resize', ()=>{
+    if(window.innerWidth !== windowWidth){
+        windowWidth = window.innerWidth; 
     }else {
-        navBar.classList.remove('navbar--onscroll');
-    } 
+        return;
+    }
+    windowWidth < 768 ? navBarLogo.classList.add('navBar_logo--onscroll') : navBarLogo.classList.remove('navBar_logo--onscroll');
+
+});
+window.onload = ()=> {
+    windowWidth < 768 ? navBarLogo.classList.add('navBar_logo--onscroll') : navBarLogo.classList.remove('navBar_logo--onscroll');
+
+    window.onscroll = ()=> {
+    
+        windowWidth < 768 ? navBarLogo.classList.add('navBar_logo--onscroll') : navBarLogo.classList.remove('navBar_logo--onscroll');
+        if(document.documentElement.scrollTop > 100 || document.body.scrollTop > 100 ){
+            navBar.classList.add('navbar--onscroll');
+  
+        } else {
+            navBar.classList.remove('navbar--onscroll');
+           // navBarLogo.classList.remove('navBar_logo--onscroll');
+            navBar.getElementsByClassName.position = 'relative';
+    
+        } 
+    
+    }
+    
 
 }
+
+
+
+
+
+// window.onscroll = ()=> {
+    
+
+//     if(document.documentElement.scrollTop > 2 || document.body.scrollTop > 2 ){
+//         navBar.classList.add('navbar--onscroll');
+//         if (windowWidth < 768) navBarLogo.style.display = 'none'  ; 
+        
+//     } else {
+//         navBar.classList.remove('navbar--onscroll');
+//         navBarLogo.style.display = 'block';
+//         navBarLogo.style.postion = 'relative';
+
+//     } 
+
+// }
